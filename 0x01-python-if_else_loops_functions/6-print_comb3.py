a155087da15fb7e98f8d7f3e6895000ff05545c5
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-06-30 00:44:09 -0700

    a program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Printing all possible different combinations of two digits in ascending order """
+"""Printing all possible different combinations of two digits in ascending order
+"""
 for a in range(0, 10):
     for b in range(a + 1, 10):
         if a == 8 and b == 9:
