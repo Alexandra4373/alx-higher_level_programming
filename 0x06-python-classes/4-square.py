35f32b61dc1d811366dc06a06703261dc5ec643a
Alexandra4373 <anaquao@st.ug.edu.gh>
2022-07-20 10:45:32 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,53 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+"""Defines a square-printing function"""
 
 
-def print_square(size):
-    """Print a square with the # character.
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
+class Square:
+    """ Square class defined by geometric shape
+        Attributes:
+            size (int): Size of square
     """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        """ initializing
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        self.__size = size
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    def area(self):
+        """
+        set square area
+        Return:
+            the current square area (int)
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        getter of size
+        Return:
+            size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter of size
+        Args:
+            size (int): size of a side of the square
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
